a665908430fee9d3a3144aa52af75197c56e39f3
Hrishabh Kumar <krhrishabh14@gmail.com>
2026-07-26 00:07:03 +0530

    Delete PROJECTS directory

--- PROJECTS/10_countdown_timer.py
+++
@@ -1,18 +0,0 @@
-"""
-10: Countdown Timer
-A terminal timer utilizing the time module.
-"""
-import time
-
-def countdown(seconds):
-    while seconds > 0:
-        mins, secs = divmod(seconds, 60)
-        timer = f"{mins:02d}:{secs:02d}"
-        print(timer, end="")
-        time.sleep(1)
-        seconds -= 1
-    print("
-Time's up!")
-
-if __name__ == "__main__":
-    countdown(3)
